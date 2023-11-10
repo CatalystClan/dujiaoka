bacd54bc7e4e80f852fa792033c8099f75e87d51
Jake Yu <jake.yu@zoom.us>
2023-11-10 14:24:39 +0800

    feat: docker 构建

---
+++ release.sh
@@ -0,0 +1,12 @@
+npx release-it
+
+# 获取package.json中的版本号
+export APP_VERSION=$(jq -r '.version' package.json)
+echo $APP_VERSION
+
+# pnpm build:web
+# pnpm build:web:rm:sourcemap
+
+# node ./docker-web/tencent.js
+
+# pnpm clean
